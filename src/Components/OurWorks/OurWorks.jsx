53ba1e44052555f30def5d86781c2795b6722ace
Sebastian Monsalve <sebastiandev16@gmail.com>
2024-07-10 20:39:01 -0500

    fix: using project data

--- src/Components/OurWorks/OurWorks.jsx
+++ src/Components/OurWorks/OurWorks.jsx
@@ -1,50 +1,56 @@
-import './OurWorks.css';
-import OurWorksData from './OurWorksData';
+import "./OurWorks.css";
+import OurWorksData from "../Portfolio_List/portfolio_listData";
 
 const OurWorks = () => {
+  const latestWorks = OurWorksData.slice(-3);
+
   return (
-    <section className='works'>
-      <article className='works-title-container'>
-        <div className='works-titles'>
-          <h2 className='section-title'>Our Works</h2>
-          <h3 className='section-subtitle works-subtitle'>Case studies</h3>
+    <section className="works">
+      <article className="works-title-container">
+        <div className="works-titles">
+          <h2 className="section-title">Our Works</h2>
+          <h3 className="section-subtitle works-subtitle">Case studies</h3>
         </div>
-        <div className='works-descriptions'>
-          <p className='works-description'>
-            Well-crafted development and design solutions are essential present products that your audience will fall in
-            love with
+        <div className="works-descriptions">
+          <p className="works-description">
+            Well-crafted development and design solutions are essential present
+            products that your audience will fall in love with
           </p>
         </div>
       </article>
-      <article className='works-card-container'>
-        {OurWorksData.map((work, index) => {
-          const cardClassName = index % 2 === 0 ? 'works-card' : 'works-card works-card-reverse';
+      <article className="works-card-container">
+        {latestWorks.map((work, index) => {
+          const cardClassName =
+            index % 2 === 0 ? "works-card" : "works-card works-card-reverse";
           return (
             <div key={index} className={cardClassName}>
-              <picture className='works-card-image'>
-                <img src={work.image} alt={work.title} draggable='false' loading='lazy' />
-                <a href={work.link} target='_blank' title='View demo'>
+              <picture className="works-card-image">
+                <img
+                  src={work.image}
+                  alt={work.alt}
+                  draggable="false"
+                  loading="lazy"
+                />
+                <a href={work.link} target="_blank" title="View demo">
                   <span>Explore</span>
                 </a>
               </picture>
-              <div className='works-card-content'>
-                <div className='works-card-category'>
-                  {work.category.map((category, index) => (
-                    <p key={index}>{category}</p>
-                  ))}
+              <div className="works-card-content">
+                <div className="works-card-category">
+                  <p>{work.categories[0]}</p>
                 </div>
-                <div className='works-card-text'>
+                <div className="works-card-text">
                   <a href={work.link}>
-                    <h4 className='works-card-title'>{work.title}</h4>
+                    <h4 className="works-card-title">{work.title}</h4>
                   </a>
-                  <p className='works-card-description'>{work.description}</p>
+                  <p className="works-card-description">{work.text}</p>
                 </div>
               </div>
             </div>
           );
         })}
       </article>
-      <a href='/portfolio' className='works-button'>
+      <a href="/portfolio" className="works-button">
         See All Cases
       </a>
     </section>
